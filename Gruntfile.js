77565612d9f9054cc64fe5841cd1f512b04efb75
Zack Katz <zack@katzwebservices.com>
2016-02-15 18:43:37 -0700

    Only minify CTCT files

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function(grunt) {
 				files: [{
 					expand: true,
 					cwd: 'js/admin',
-					src: ['**/*.js','!**/*.min.js'],
+					src: ['js/*.js','!**/*.min.js'],
 					dest: 'js/admin',
 					ext: '.min.js'
 				}]
