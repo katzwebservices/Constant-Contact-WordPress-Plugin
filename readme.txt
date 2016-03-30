86190dcd394de04e3236afe2965872bcfea369c2
Zack Katz <zack@katzwebservices.com>
2016-03-30 01:36:02 -0600

    Version bump

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: katzwebdesign, katzwebservices
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=zackkatz%40gmail%2ecom&item_name=Constant%20Contact%20API%20Plugin&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8
 Tags: mail, email, newsletter, Constant Contact, plugin, sidebar, widget, mailing list, API, email marketing, newsletters, form, forms, event, events, event marketing
 Requires at least: 3.3
-Tested up to: 4.3
-Stable tag: 3.1.13
+Tested up to: 4.4.2
+Stable tag: 4.0
 License: GPLv2 or later
 
 Integrate Constant Contact into your website with this full-featured plugin.
--- views/admin/view.php-compatibility-notice.php
+++ views/admin/view.php-compatibility-notice.php
@@ -1,15 +1,13 @@
 <div class="inline" style="border: 1px solid #ccc; margin:10px 0; padding: 10px; border-radius:5px; background-color:white; ">
 	<h2 class="cc_logo"><a class="cc_logo"><?php _e('Constant Contact', 'constant-contact-api'); ?></a></h2>
 	<h3><?php _e('Please upgrade your website\'s PHP Version', 'constant-contact-api'); ?></h3>
-	<p><?php _e('Starting with Version 3.2, <strong>the Contstant Contact Plugin requires PHP Version 5.4 or higher</strong>. Please contact your hosting provider support and ask them to upgrade your server.', 'constant-contact-api'); ?></p>
-	<h3><?php _e('Can\'t upgrade? Want to go back?', 'constant-contact-api'); ?></h3>
-	<p><?php _e('If you would like to revert to the previous version, do this:', 'constant-contact-api'); ?></p>
-	<ol>
-		<li><a href="http://downloads.wordpress.org/plugin/constant-contact-api.2.4.1.zip"><?php esc_html_e('Download Version 2.4.1', 'constant-contact-api'); ?></a></li>
-		<li><?php _e(sprintf('<a href="%s" onclick="return confirm(\'This will hide these instructions; are you sure you know what to do next?\');">De-activate this Plugin</a>',
-		    wp_nonce_url(admin_url('plugins.php?action=deactivate&plugin='.plugin_basename( CTCT_FILE )), 'deactivate-plugin_'.plugin_basename( CTCT_FILE ))
-		), 'constant-contact-api'); ?></li>
-		<li><?php printf( esc_html__('Delete this plugin ("Constant Contact Plugin for WordPress") by clicking the "Delete" link next to the plugin on the %sPlugins Page%s.', 'constant-contact-api'), '<a href="'.admin_url('plugins.php').'">', '</a>' ); ?></li>
-		<li><?php _e('Upload the Version 2.4.1 file in Plugins > Add New > Upload', 'constant-contact-api'); ?></li>
-	</ol>
+	<p><?php _e('Starting with Version 4.0, <strong>the Constant Contact Plugin requires PHP Version 5.5 or higher</strong>. Please contact your hosting provider support and ask them to upgrade your server.', 'constant-contact-api'); ?></p>
+	<p><?php _e('We apologize for the inconvenience, but we had no choice.', 'constant-contact-api'); ?></p>
+	<ul>
+		<li><a href="http://www.inmotionhosting.com/support/website/php/how-to-change-the-php-version-your-account-uses">InMotion Hosting</a></li>
+		<li><a href="http://support.hostgator.com/articles/cpanel/php-configuration-plugin">HostGator</a></li>
+		<li><a href="https://my.bluehost.com/cgi/help/447">Bluehost</a></li>
+		<li><a href="https://www.godaddy.com/help/view-or-change-your-php-version-16090">GoDaddy</a></li>
+		<li><a href="https://www.siteground.com/kb/how_to_have_different_php__mysql_versions/">SiteGround</a></li>
+	</ul>
 </div>
